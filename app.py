97671ce769cd3bcbc3dc4456e307e79787a1874b
Budnampet Ramanudom class of 2018 <bramanud@tempest.wellesley.edu>
2017-11-11 18:25:59 -0500

    lol let's just use bootstrap

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, url_for
 import os, sys
 import MySQLdb
 import dbconn2
@@ -7,21 +7,28 @@ app = Flask(__name__)
 
 def getConn():
   DSN = dbconn2.read_cnf()
-  DSN['db'] = 'nho_db' #LOSER
+  DSN['db'] = 'bramanud_db' #LOSER
   return dbconn2.connect(DSN)
 
 @app.route('/welcome/', methods = ['GET'])
-def welcomePage():
-  return app.send_static_file("index.html")
-@app.route('/welcome/src/x-app.html', methods = ['GET'])
-def appPage():
-  return app.send_static_file("src/x-app.html")
+def welcome():
+  if request.method == 'GET':
+    return render_template('index.html')
+# @app.route('/css/', methods = ['GET'])
+# def appPage():
+#   return app.send_static_file("src/x-app.html")
 #@app.route('/addReview/', methods= ['GET','POST'])
 #@app.route('/FAQ/', methods = ['GET'])
 #@app.route('/About/', methods = ['GET'])
 #@app.route('/Tags/', methods = ['GET'])
 
-app.secret_key = 'petEqualsLoser'
+
+@app.route('/welcome/static/jumbotron.css')
+def send_static():
+    return url_for('static', filename='jumbotron.css')
+
+
+app.secret_key = 'nancyEqualsLoser'
 
 if __name__ == '__main__':
   app.debug == True
