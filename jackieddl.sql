7c67fa29dd9cf82814008bbe957e02c29c276029
Nancy Ho <nancyho@mit.edu>
2017-11-11 21:36:37 -0500

    sdfdsjfl

--- jackieddl.sql
+++ jackieddl.sql
@@ -67,10 +67,10 @@ create table experiences (
 
 ENGINE = InnoDB;
 
-insert into experiences values
-       (1, 'Target'),
-       (5, 'Target'),
-       (6, 'Google');
+-- insert into experiences values
+--        (1, 'Target'),
+--        (5, 'Target'),
+--        (6, 'Google');
 
 
 
