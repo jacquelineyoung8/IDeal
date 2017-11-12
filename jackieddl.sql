653c19996b81b07f84295b24e2dbdfcb8f7cf56b
bramanudom <bramanud@tempest.wellesley.edu>
2017-11-12 11:08:58 -0500

    im sorry ahhh

--- jackieddl.sql
+++ jackieddl.sql
@@ -1,22 +1,27 @@
 use bramanud_db;
+SET FOREIGN_KEY_CHECKS = 0;
 drop table if exists experiences;
 drop table if exists tags;
 drop table if exists reviews;
-drop table if exists keywords;
 drop table if exists companies;
+drop table if exists keywords;
+SET FOREIGN_KEY_CHECkS = 1;
+
+create table companies (
+        c_name varchar(50) primary key
+)
+ENGINE = InnoDB;
 
 create table reviews (
-       r_id int primary key,
-       post varchar(500),
+       r_id varchar(50) primary key,
+       c_name varchar(50) not null,
+       post varchar(50),
        pos_sentiment boolean,
-       job_title varchar(500)
+       job_title varchar(500),
+       foreign key (c_name) references companies(c_name) on delete restrict
 )
 
 ENGINE = InnoDB;
-
-insert into reviews values
-       (1, 'this racist company is very bad', false, 'senior engineer'),
-       (2, 'they are mean', false, 'product engineer');
 
 
 create table keywords (
@@ -25,42 +30,18 @@ create table keywords (
 
 ENGINE = InnoDB;
 
-insert into keywords values
-       ('sexism'),
-       ('terrible'),
-       ('sad');
-
-
 create table tags (
-       r_id int,
-       t_label varchar(50),
+       r_id varchar(50) not null,
+       t_label varchar(50) not null,
        foreign key (r_id) references reviews(r_id) on delete restrict,
        foreign key (t_label) references keywords(t_label) on delete restrict
 )
 
 ENGINE = InnoDB;
 
-insert into tags values
-       (1, 'sexism'),
-       (1, 'terrible'),
-       (2, 'sad');
-
-
-create table companies (
-	c_name varchar(50) primary key
-)
-
-ENGINE = InnoDB;
-
-insert into companies values
-       ('Target'),
-       ('Google'),
-       ('Home Depot');
-
-
 create table experiences (
-    r_id int,
-	c_name varchar(50),
+        r_id varchar(50) not null,
+	c_name varchar(50) not null,
 	foreign key (r_id) references reviews(r_id) on delete restrict,
 	foreign key (c_name) references companies(c_name) on delete restrict
 )
